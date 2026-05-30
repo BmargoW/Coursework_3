016f86113e7c852a02775ceceef4d8586ab96d4a
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-05-31 02:12:42 +0300

    add db_manager.py

--- src/db_manager.py
+++ src/db_manager.py
@@ -10,9 +10,12 @@ class BDContact(ABC):
     @abstractmethod
     def get_companies_and_vacancies_count(self):
         pass
-
-    # def flight_data(self):
-    #     pass
+    def get_all_vacancies(self):
+        pass
+    def get_avg_salary(self):
+        pass
+    def get_vacancies_with_higher_salary(self):
+        pass
 
 class DatabaseSelection(BDContact):
     """Конструктор для подключения к БД"""
